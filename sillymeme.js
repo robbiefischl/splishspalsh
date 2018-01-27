6e0766466749e9b82fbd07ea795c3859ae5fbcc0
robbiefischl <35855251+robbiefischl@users.noreply.github.com>
2018-01-27 13:26:16 -0500

    #1

---
+++ sillymeme.js
@@ -0,0 +1,13 @@
+var name="lit"; 
+console.log(name);
+if(name ==lit){
+    alert("you are lit");
+}else if(name ==jef){
+    alert("you aint lit");
+}
+
+var current = 3;
+for(var limit=100;<100;current++){
+ console.log (current);
+
+}
